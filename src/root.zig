a020a17e9dc89c2a714dc952f7d6f188b97df521
0xdeb7ef <45214659+0xdeb7ef@users.noreply.github.com>
2025-09-21 01:04:48 -0700

    added `Message` and `Device` imports to root module

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,4 @@
 pub const Client = @import("Client.zig").Client;
+pub const Message = @import("Message.zig");
+pub const Device = @import("Device.zig").Device;
 pub const getIDFromAppLoad = @import("Client.zig").getIDFromAppLoad;
-
-const std = @import("std");
