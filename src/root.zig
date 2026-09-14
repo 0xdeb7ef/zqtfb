ef5db9738c78e1a6581ec5c00d94701e50633742
Interloper <45214659+0xdeb7ef@users.noreply.github.com>
2026-09-13 23:55:01 -0700

    Woops, forgot to export Message

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,3 @@
 pub const Client = @import("Client.zig").Client;
 pub const Device = @import("Device.zig").Device;
-pub const getIDFromAppLoad = @import("Client.zig").getIDFromAppLoad;
-
-// pub const Message = @import("Message.zig");
+pub const Message = @import("Message.zig");
