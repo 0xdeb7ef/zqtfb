d42248e4e61602b19f24db5b0f75e1e9294665f0
Interloper <45214659+0xdeb7ef@users.noreply.github.com>
2026-09-13 23:56:42 -0700

    how did i even delete this line, rip

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,4 @@
 pub const Client = @import("Client.zig").Client;
 pub const Device = @import("Device.zig").Device;
+pub const getIDFromAppLoad = @import("Client.zig").getIDFromAppLoad;
 pub const Message = @import("Message.zig");
